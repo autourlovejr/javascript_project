262e771e3b4580ceae6edba240cd42b858a0619e
Autour Love Jr <autourlovejr@yahoo.com>
2021-03-16 20:40:20 -0400

    small site changes

--- javascript.js
+++ javascript.js
@@ -13,15 +13,15 @@ if (Henry.includes(search.toLowerCase())) {
 
 document.querySelector('main').innerHTML = `<p>${message}</p>`;
 
-let Rando = Math.floor( Math.random() * 6) + 1;
+// let Rando = Math.floor( Math.random() * 6) + 1;
 
-console.log(`Enjoy this random number ${Rando}`);
+// console.log(`Enjoy this random number ${Rando}`);
 
-const h2 = document.querySelector('h2');
-let html = '';
+// const h2 = document.querySelector('h2');
+// let html = '';
 
-for ( let i = 1; i <= 10; i++ ) {
-    html += `<div>${i}</div>`;
-}
+// for ( let i = 1; i <= 10; i++ ) {
+//     html += `<div>${i}</div>`;
+// }
 
-h2.innerHTML = html;
+// h2.innerHTML = html;
